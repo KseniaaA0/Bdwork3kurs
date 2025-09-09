3132f6d1427da3f0e6bed1b7d15d888f81b26dc6
Ksenia <112938243+KseniaaA0@users.noreply.github.com>
2025-09-09 17:58:26 +0700

    Update Процедура_для_оформление_нового_заказа.sql

--- FinalWork/Хранимые процедуры/Процедура_для_оформление_нового_заказа.sql
+++ FinalWork/Хранимые процедуры/Процедура_для_оформление_нового_заказа.sql
@@ -10,3 +10,17 @@ DECLARE
     v_order_id INT;
     v_status_id INT;
 BEGIN
+
+
+    SELECT status_id INTO v_status_id 
+    FROM order_statuses 
+    WHERE name = 'Оформлен';
+    
+
+    INSERT INTO orders (customer_id, employee_id, status_id, payment_method_id, total_amount, shipping_address)
+    VALUES (p_customer_id, p_employee_id, v_status_id, p_payment_method_id, 0, p_shipping_address)
+    RETURNING order_id INTO v_order_id;
+    
+    RAISE NOTICE 'Заказ №% успешно создан', v_order_id;
+END;
+$$;
